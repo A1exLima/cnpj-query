137a2fbba51a21ea7944dfcbdab7337bf4351be5
Alex Lima <alex.lima_dev@outlook.com>
2024-08-01 13:09:04 -0300

    Campo para pesquisar por CNPJ na página de histórico
    - Lógica para filtro de pesquisar pelo CNPJ
    - html e css

--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -1,34 +1,72 @@
-import { useEffect, useState } from 'react'
 import { Content, HistoryContainer } from './style'
+
+import { useEffect, useState } from 'react'
+
 import { OrganizeDataByBusinessRuleProps } from '../../hooks/organizeData'
+
 import { CardHistory } from '../../components/cardHistory'
 
-// Componente que exibe o histórico de consultas de CNPJ.
+import { IoSearchSharp } from 'react-icons/io5'
+
+// Componente History para exibir todas as empresas pesquisadas pelo cnpj
 export function History() {
-  // Estado para armazenar os dados do histórico
+  // Estado para armazenar os dados do banco de dados
   const [dataBase, setDataBase] = useState<OrganizeDataByBusinessRuleProps[]>(
     [],
   )
+  // Estado para armazenar os dados filtrados
+  const [filteredData, setFilteredData] = useState<
+    OrganizeDataByBusinessRuleProps[]
+  >([])
+  // Estado para armazenar o valor da busca do CNPJ
+  const [searchCnpj, setSearchCnpj] = useState<string>('')
 
-  // Efeito para carregar os dados do localStorage ao montar o componente
+  // Função para atualizar o estado do CNPJ de busca conforme o usuário digita
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchCnpj(event.target.value)
+  }
+
+  // useEffect para carregar os dados armazenados no localStorage quando o componente é montado
   useEffect(() => {
-    // Verifica se há dados salvos no localStorage
     const storedData = localStorage.getItem('cnpjQuery-database')
     if (storedData) {
-      // Converte os dados salvos de volta para o formato de array de objetos
       const parsedData = JSON.parse(storedData)
       setDataBase(parsedData.reverse())
     }
   }, [])
 
+  // useEffect para filtrar os dados com base no valor do searchCnpj
+  useEffect(() => {
+    if (searchCnpj.trim() === '') {
+      setFilteredData(dataBase)
+    } else {
+      const filtered = dataBase.filter((item) =>
+        item.cnpj.toLowerCase().includes(searchCnpj.toLowerCase()),
+      )
+      setFilteredData(filtered)
+    }
+  }, [searchCnpj, dataBase])
+
   return (
     <HistoryContainer>
       <Content>
         <h2>Histórico de consultas</h2>
+        <div className="container-search">
+          <label htmlFor="search">
+            <IoSearchSharp />
+          </label>
+          <input
+            id="search"
+            type="search"
+            placeholder="Pesquisar por CNPJ"
+            value={searchCnpj}
+            onChange={handleSearchChange}
+          />
+        </div>
         <div>
-          {/* Mapeia os dados do histórico e renderiza cada CardHistory */}
-          {dataBase &&
-            dataBase.map((card) => <CardHistory key={card.id} data={card} />)}
+          {filteredData.map((card) => (
+            <CardHistory key={card.id} data={card} />
+          ))}
         </div>
       </Content>
     </HistoryContainer>
--- src/pages/history/style.ts
+++ src/pages/history/style.ts
@@ -14,10 +14,6 @@ export const Content = styled.article`
   flex-direction: column;
   justify-content: space-between;
 
-  @media (max-width: 768px) {
-    padding: 2rem;
-  }
-
   margin-bottom: 4rem;
 
   > h2 {
@@ -27,9 +23,58 @@ export const Content = styled.article`
     padding-bottom: 4rem;
   }
 
+  .container-search {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 1.5rem;
+    background: ${({ theme }) => theme['Chinese-White']};
+    margin-bottom: 2rem;
+    border-radius: 1.5rem;
+    padding: 1.5rem;
+    box-shadow: rgba(0, 0, 0, 0.4) 0px 0px 10px;
+
+    transition: filter 0.2s ease-in-out;
+
+    &:hover {
+      filter: brightness(120%);
+    }
+
+    > label {
+      cursor: pointer;
+      > svg {
+        display: flex;
+        font-size: 3rem;
+        color: ${({ theme }) => theme['Slate-Blue']};
+      }
+    }
+
+    > input {
+      width: inherit;
+      border: none;
+      background-color: ${({ theme }) => theme['Chinese-White']};
+
+      font-size: 1.6rem;
+      font-weight: 400;
+      font-family: var(--font-Roboto);
+      color: ${({ theme }) => theme['Charcoal-Blue']};
+
+      &::placeholder {
+        color: ${({ theme }) => theme['Charcoal-Blue']};
+        font-weight: 500;
+        opacity: 75%;
+      }
+    }
+  }
+
   > div {
     display: flex;
     flex-direction: column;
     gap: 2rem;
+  }
+
+  @media (max-width: 768px) {
+    padding: 2rem;
   }
 `
